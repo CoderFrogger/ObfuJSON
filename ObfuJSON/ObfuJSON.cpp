a5d957baa130258cc7588da72a51daf60d74e3ff
Iggy de Biggy <ignashisp@gmail.com>
2023-06-18 13:45:56 +0300

    Added I/O in the apps privates for the obfuscator to use

--- ObfuJSON/ObfuJSON.cpp
+++ ObfuJSON/ObfuJSON.cpp
@@ -1,8 +1,65 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+class JSONObfuscator
+{
+public:
+	JSONObfuscator(const string jsonInputPath, const string jsonOutputPath)
+	{
+		this->jsonInputPath = jsonInputPath;
+		this->jsonOutputPath = jsonOutputPath;
+	}
+
+private:
+		string jsonInputPath;
+		string jsonOutputPath;
+
+		void readInput() {
+			ifstream inputFile(jsonInputPath);
+			if (!inputIsOpen(inputFile)) return;
+
+			string line;
+			while (!inputFile.eof()) {
+				getline(inputFile, line);
+			}
+		}
+
+		void writeOutput(const string obfuscatedJSON) {
+			ofstream outputFile(jsonOutputPath);
+			if (!outputIsCreated(outputFile)) return;
+
+			outputFile << obfuscatedJSON;
+			outputFile.close();
+
+			cout << "Output file created successfully." << endl;
+		}
+
+		bool inputIsOpen(ifstream& inputFile) {
+			if (!inputFile.is_open()) {
+				cerr << "Error opening input file." << endl;
+				return false;
+			}
+		}
+
+		bool outputIsCreated(ofstream& outputFile) {
+			if (!outputFile) {
+				cerr << "Error creating output file." << endl;
+				return false;
+			}
+		}
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	string jsonInputPath, jsonOutputPath= "output.json";
+
+	cout << "Enter JSON file name: ";
+	cin >> jsonInputPath;
+
+	JSONObfuscator obfuscator(jsonInputPath, jsonOutputPath);
+
+	return 0;
 }
