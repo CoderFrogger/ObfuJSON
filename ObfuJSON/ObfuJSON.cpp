7ddb6e6cacff0951f0977f4dba8396aab7e9dbec
Iggy de Biggy <ignashisp@gmail.com>
2023-06-19 19:17:17 +0300

    Added obfuscation functionality

--- ObfuJSON/ObfuJSON.cpp
+++ ObfuJSON/ObfuJSON.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
+#include <sstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -15,7 +18,10 @@ public:
 
 	void obfuscate() {
 		string obfuscatedJSON, normalJSON;
+
 		readInput(normalJSON);
+		obfuscatedJSON = normalToObfuscatedJSON(normalJSON);
+
 		writeOutput(obfuscatedJSON);
 	}
 
@@ -23,13 +29,13 @@ private:
 		string jsonInputPath;
 		string jsonOutputPath;
 
-		void readInput(string normalJSON) {
+		void readInput(string& normalJSON) {
 			ifstream inputFile(jsonInputPath);
 			if (!inputIsOpen(inputFile)) return;
 
 			string line;
-			while (!inputFile.eof()) {
-				getline(inputFile, line);
+			while (getline(inputFile, line)) {
+				normalJSON += line + "\n";
 			}
 		}
 
@@ -48,6 +54,7 @@ private:
 				cerr << "Error opening input file." << endl;
 				return false;
 			}
+			return true;
 		}
 
 		bool outputIsCreated(ofstream& outputFile) {
@@ -55,6 +62,42 @@ private:
 				cerr << "Error creating output file." << endl;
 				return false;
 			}
+			return true;
+		}
+
+		string stringToUnicodeEscape(const string normalJSON){
+			stringstream unicodeEscape;
+			
+			for (const char& i : normalJSON) {
+				unicodeEscape << "\\u" << hex << setw(4) << setfill('0') << (int)i;
+			}
+
+			return unicodeEscape.str();
+		}
+
+		string normalToObfuscatedJSON(const string &normalJSON) {
+			string line, obfuscatedJSON;
+
+			size_t possition = 0, start=0, end=0;
+
+			while (possition < normalJSON.size()) {
+				start = normalJSON.find_first_of("\"", possition+1);
+
+				if (start != string::npos) {
+					end = normalJSON.find_first_of("\"", start + 1);
+
+					if (end != string::npos) {
+						obfuscatedJSON += normalJSON.substr(possition, start - possition + 1)
+							+ stringToUnicodeEscape(normalJSON.substr(start+1, end - start - 1));
+
+						possition = end;
+						continue;
+					}
+				}
+				obfuscatedJSON += normalJSON.substr(possition);
+				break;
+			}
+			return obfuscatedJSON;
 		}
 };
 
@@ -66,6 +109,8 @@ int main()
 	cin >> jsonInputPath;
 
 	JSONObfuscator obfuscator(jsonInputPath, jsonOutputPath);
+
+	obfuscator.obfuscate();
 
 	return 0;
 }
